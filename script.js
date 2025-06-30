3ef8f94b5ad88359401d3a81b45b17f338351597
Theo <theophilus.shelton.jr@gmail.com>
2025-06-29 23:44:38 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Hollow  Pedals site loaded and ready!");
+  // You can add interactive gallery, shop cart, or animation here later
+});
